1bddc09e9bcd70a219bad8e1dc9997524b138d93
james2O <jamowaind@gmail.com>
2024-06-21 15:47:44 +0100

    and the rest

--- Shred/src/App.tsx
+++ Shred/src/App.tsx
@@ -1,18 +1,8 @@
-import ListGroup from "./components/ListGroup";
 import Sidebar from "./components/Sidebar";
 import TitleBar from "./components/TitleBar";
+import MapStage from "./components/MapStage";
 
-let items = [
-  'Brechfa',
-  'Abergorlech',
-  'Llansawel',
-  'Llanfynydd'
-];
-const handleSelectItem = (item: string) => {
-  console.log(item);
-}
 function App() {
-  //return <div><ListGroup items={items} heading="Brechfa Forest Villages" onSelectItem={handleSelectItem}/></div>;
   return (
     <div className="container">
       <div className="row">
@@ -21,7 +11,7 @@ function App() {
       <div className="row">
         <Sidebar />
       </div>
-      <div className = "row">
+      <div className="row">
         <MapStage />
       </div>
     </div>
--- Shred/src/components/MapStage.tsx
+++ Shred/src/components/MapStage.tsx
@@ -1,15 +1,31 @@
-import { useState } from 'react';
-import '../index.css';
+import { useState, useEffect, useRef } from 'react';
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiamFtZXMybyIsImEiOiJjbHhvbnJlMWQwZXByMnNxcnA4dGw3bWxyIn0.5M3i7YKGorD0Dm2mRk-9UQ';
 
 function MapStage() {
-  var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
-  mapboxgl.accessToken = 'pk.eyJ1IjoiamFtZXMybyIsImEiOiJjbHhvbnJlMWQwZXByMnNxcnA4dGw3bWxyIn0.5M3i7YKGorD0Dm2mRk-9UQ';
-  var map = new mapboxgl.Map({
-    container: 'mapStage',
-    style: 'mapbox://styles/mapbox/streets-v11'
+
+  const mapContainer = useRef<any>(null);
+  const map = useRef<mapboxgl.Map | null>(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [lng, lat],
+      zoom: zoom
+    });
   });
-    return (
-        <div id="mapStage" />
-    );
+
+  return (
+    <div>
+      <div ref={mapContainer} className="map-container" />
+    </div>
+      
+  );
 }
 export default MapStage;
